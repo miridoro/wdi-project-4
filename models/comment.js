cd4b4ac9ec202596bddad1a319ccd3ce2b3e7d34
miridoro <miriam.wodrich@gmail.com>
2016-10-24 11:12:54 +0100

    get all posts to show

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+const mongoose   = require ("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  text:       { type: String, trim: true, required: true },
+  user:       { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  post:       { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+}, {
+  timestamps: true
+});
+
+module.exports   = mongoose.model("Comment", blogSchema);
--- models/post.js
+++ models/post.js
@@ -1,12 +1,12 @@
 const mongoose   = require ("mongoose");
 
 const postSchema = new mongoose.Schema({
-  text:       { type: String, trim: true },
-  image:      { type: String, trim: true },
-  time:       { type: Date }, // required: true
-  content:    { type: String, trim: true},
-  user:       { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  // comment:   [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  text:              { type: String, trim: true },
+  image:             { type: String, trim: true },
+  time:              { type: Date }, // required: true
+  contentwysiwyg:    { type: String, trim: true},
+  user:              { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  // comments:   [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 }, {
   timestamps: true
 });
