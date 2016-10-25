469403a99c3958ba6aec1bc30912e2d2d0103ce1
miridoro <miriam.wodrich@gmail.com>
2016-10-25 17:04:10 +0100

    got textAngular to work

--- models/post.js
+++ models/post.js
@@ -1,12 +1,11 @@
 const mongoose   = require ("mongoose");
 
 const postSchema = new mongoose.Schema({
-  text:              { type: String, trim: true },
-  image:             { type: String, trim: true },
-  time:              { type: Date }, // required: true
-  contentwysiwyg:    { type: String, trim: true},
-  user:              { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  comments:         [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  title:    { type: String, trim: true },
+  image:    { type: String, trim: true },
+  content:  { type: String, trim: true},
+  user:     { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 }, {
   timestamps: true
 });
--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,5 @@ angular
     //library that helps us decode the jwt to work out what the user name is
     "angular-jwt",
     "ngTouch",
+    'textAngular'
   ]);
-
-
-['textAngular']
