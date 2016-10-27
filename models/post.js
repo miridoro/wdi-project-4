14d2b27a893be532b2c5213798a3896d72d8671a
miridoro <miriam.wodrich@gmail.com>
2016-10-27 12:27:11 +0100

    styling posts pages

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ const mongoose   = require ("mongoose");
 const postSchema = new mongoose.Schema({
   title:    { type: String, trim: true },
   image:    { type: String, trim: true },
+  about:    { type: String, trim: true },
   content:  { type: String, trim: true},
   user:     { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
   // comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
